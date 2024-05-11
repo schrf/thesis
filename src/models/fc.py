8d38a2eb2ffdc9c7bd33ef6631fd947177a3dbf4
Felix Schroeder <felix.schroeder@tum.de>
2024-05-11 11:30:06 +0200

    the two hidden sizes can now be separately be defined

--- src/models/fc.py
+++ src/models/fc.py
@@ -3,19 +3,19 @@ from torch import nn
 import torch.nn.functional as F
 
 class VAE(nn.Module):
-    def __init__(self, input_size, hidden_size, z_size):
+    def __init__(self, input_size, hidden_one_size, hidden_two_size, z_size):
         super().__init__()
 
         # encoder
-        self.fc1 = nn.Linear(input_size, hidden_size)
-        self.fc2 = nn.Linear(hidden_size, int(hidden_size / 2))
-        self.fc_mu = nn.Linear(int(hidden_size / 2), z_size)
-        self.fc_sigma = nn.Linear(int(hidden_size / 2), z_size)
+        self.fc1 = nn.Linear(input_size, hidden_one_size)
+        self.fc2 = nn.Linear(hidden_one_size, hidden_two_size)
+        self.fc_mu = nn.Linear(hidden_two_size, z_size)
+        self.fc_sigma = nn.Linear(hidden_two_size, z_size)
 
         # decoder
-        self.fc3 = nn.Linear(z_size, int(hidden_size / 2))
-        self.fc4 = nn.Linear(int(hidden_size / 2), hidden_size)
-        self.fc5 = nn.Linear(hidden_size, input_size)
+        self.fc3 = nn.Linear(z_size, hidden_two_size)
+        self.fc4 = nn.Linear(hidden_two_size, hidden_one_size)
+        self.fc5 = nn.Linear(hidden_one_size, input_size)
 
 
     def encode(self, x):

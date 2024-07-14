7e41b0b54a11f821090144252c5d3f55b19d18c3
Felix Schroeder <felix.schroeder@tum.de>
2024-07-14 15:41:20 +0200

    simplified plotting

--- src/data_visualization.py
+++ src/data_visualization.py
@@ -1,106 +1,85 @@
-from pacmap import PaCMAP
+import os.path
+
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 import phate
 
-from src.data_transformation import z_score_normalization_rowwise, z_score_normalization_columnwise, filter_variance
+def scatter_plot_classes(Y, metadata, title=None, output_dir=None):
+    """
+    creates a scatter plot visualization where each sample is colored by class
+    :param data: the gene expression data as pandas dataframe or 2d numpy array
+    :param metadata: a pandas Series or 1d numpy array containing metadata with classes
+    :param title: optional title, if None the Series title is used
+    :param output_dir: path to save the plot. If None plot.show() will be called
+    """
 
-metadata_columns = ['primary_disease', 'gender', 'age', 'dataset']
-
-def combined_data_pacmap(data, metadata, transform):
-    if transform is not None:
-        if transform.get("genes_filter") is not None:
-            genes_list = transform.get("genes_filter")
-            data = data[genes_list]
-
-        if transform.get("z_score") == "per_gene":
-            data = z_score_normalization_columnwise(data, data.columns)
-        elif transform.get("z_score") == "per_sample":
-            data = z_score_normalization_rowwise(data, data.columns)
-
-    _combined_data_pacmap(data, metadata, transform)
-
-
-def only_data_columns(data):
-    extended_metadata_columns = metadata_columns
-    if 'normalized_age' in data.columns:
-        extended_metadata_columns.append('normalized_age')
-
-    data_columns = [col for col in data.columns if
-                    col not in extended_metadata_columns]
-    return data_columns
-
-
-def _combined_data_pacmap(data, metadata, transform):
-    # Step 1: fill NaN values and normalize the age column
-    metadata['gender'].fillna('unknown', inplace=True)
-    data = data.fillna(0)
-    metadata.loc[:, 'normalized_age'] = (metadata['age'] - metadata['age'].min()) / (metadata['age'].max() - metadata['age'].min())
-
-    # Initialize Pacmap with PaCMAP() or PHATE with phate.PHATE(n_jobs=-2)
-    model = phate.PHATE(n_jobs=-2)
-    model_name = "PHATE"
-
-    # extract the preprocessing information for showing it in the plots
-    transform_string = "transformation: "
-    if transform is None:
-        transform_string += "None"
-    else:
-        genes_transform = "no gene filter"
-        if transform.get("genes_filter") is not None:
-            genes_transform = f"{len(transform.get('genes_filter'))} most variant genes"
-
-        z_score_transform = "no z_score normalization"
-        if transform.get("z_score") is not None:
-            z_score_transform = f"{transform.get('z_score')} z-score normalization"
-
-    # Fit the model
-    Y = model.fit_transform(data)
-
-    # Visualizations
-    create_pacmap_visualization(metadata, Y, 'primary_disease', f'{model_name} Visualization of: Primary Disease')
-    create_pacmap_visualization(metadata, Y, 'gender', f'{model_name} Visualization of: Gender')
-    create_pacmap_visualization(metadata, Y, 'age', f'{model_name} Visualization of: Age')
-    create_pacmap_visualization(metadata, Y, 'dataset', f'{model_name} Visualization of: Dataset')
-
-def create_pacmap_visualization(data, Y, metadata_column, title):
-    """Helper function to create Pacmap visualization"""
-
-    # Plot the Pacmap visualization
-    plt.figure(figsize=(16, 12))
-
+    # Plot the PHATE transformed data
+    plt.figure(figsize=(24, 18))
     plt.rcParams['font.size'] = 16
 
-    dot_size = 2
-    
-    # Color coding
-    if metadata_column == 'age':
-        scatter = plt.scatter(Y[:, 0], Y[:, 1], c=data['normalized_age'], cmap='viridis', s=dot_size)
-        plt.colorbar(scatter, label='Relative Age')
-    else:
-        categories = data[metadata_column].unique()
+    dot_size = 300 * (1 / len(metadata)**0.5)
 
-        # Generate distinct colors for each category
-        colormap_one = plt.colormaps.get_cmap('tab20')
-        colors_one = [colormap_one(i) for i in range(20)]
-        colormap_two = plt.colormaps.get_cmap('tab20b')
-        colors_two = [colormap_two(i) for i in range(20)]
-        tab20_duplicated = colors_one + colors_two
-        colors = tab20_duplicated
-                
-        for i, category in enumerate(categories):
-            mask = data[metadata_column] == category
-            color = colors[i]
-            
-            plt.scatter(Y[mask, 0], Y[mask, 1], color=color, label=category, s=dot_size)
-        
-        plt.legend(title=metadata_column, markerscale=5, bbox_to_anchor=(1.05, 1), loc='upper left')
-    
+    # Color coding
+    categories = metadata.unique()
+
+    if len(categories) > 40:
+        colors = generate_pastel_colors(len(categories))
+    else:
+        colors = tab_forty()
+
+    #plotting
+    for i, category in enumerate(categories):
+        mask = metadata == category
+        color = colors[i]
+
+        plt.scatter(Y[mask, 0], Y[mask, 1], color=color, label=category, s=dot_size)
+
+    plt.legend(title=metadata.name, markerscale=5, bbox_to_anchor=(1.05, 1), loc='upper left')
+    if title is None:
+        title = metadata.name
     plt.title(title, fontsize=22)
     plt.xlabel('Dimension 1')
     plt.ylabel('Dimension 2')
-    plt.show()
+    if output_dir is None:
+        plt.show()
+    else:
+        if not os.path.exists(output_dir):
+            os.mkdir(output_dir)
+        file_path = output_dir + title
+        print("saving plot in " + file_path)
+        plt.savefig(file_path, bbox_inches='tight')
 
+def scatter_plot_gradient(Y, metadata, title=None, output_dir=None):
+    """
+    creates a scatter plot visualization where samples are colored by a color gradient
+    :param Y: the output of PHATE's model.fit_transform(data)
+    :param metadata: a pandas Series or 1d numpy array containing metadata with numbers
+    :param title: optional title, if None the Series title is used
+    :param save: path to save the plot. If None plot.show() will be called
+    """
+
+    # Plot the PHATE transformed data
+    plt.figure(figsize=(24, 18))
+    plt.rcParams['font.size'] = 16
+
+    dot_size = 300 * (1 / len(metadata)**0.5)
+
+    scatter = plt.scatter(Y[:, 0], Y[:, 1], c=metadata, cmap='viridis', s=dot_size)
+    plt.colorbar(scatter)
+
+    if title is None:
+        title = metadata.name
+    plt.title(title, fontsize=22)
+    plt.xlabel('Dimension 1')
+    plt.ylabel('Dimension 2')
+    if output_dir is None:
+        plt.show()
+    else:
+        if not os.path.exists(output_dir):
+            os.mkdir(output_dir)
+        file_path = output_dir + title
+        print("saving plot in " + file_path)
+        plt.savefig(file_path, bbox_inches='tight')
 
 def pairwise_comparison(original, reconstructed, sample_names=None):
     """
@@ -108,7 +87,7 @@ def pairwise_comparison(original, reconstructed, sample_names=None):
     :param original: NxD np array or pd DataFrame of original gene expression data
     :param reconstructed: NxD np array or pd DataFrame of reconstructed gene expression data
     :param title: A optional string that contains the plot title
-    :param sample_names: a list of strings with the title of each sample. len(sample_names) should be 8
+    :param sample_names: a list of strings with the title of each sample. len(sample_names) should be equal to N
     :return: None
     """
 
@@ -170,3 +149,11 @@ def generate_pastel_colors(num_colors):
 
     return colors
 
+def tab_forty():
+    # Generate distinct colors for each category
+    colormap_one = plt.colormaps.get_cmap('tab20')
+    colors_one = [colormap_one(i) for i in range(20)]
+    colormap_two = plt.colormaps.get_cmap('tab20b')
+    colors_two = [colormap_two(i) for i in range(20)]
+    tab20_duplicated = colors_one + colors_two
+    return tab20_duplicated

724b1eb5231c218b5d2e66174b92319456836ab0
Felix Schroeder <felix.schroeder@tum.de>
2024-08-08 14:06:11 +0200

    changed from GradNorm to fixed weights

--- src/training.py
+++ src/training.py
@@ -21,20 +21,14 @@ input_dim = 3350
 latent_dim = 64
 lr = 0.0001
 lr_scaling = lr
-kl_loss_weight = 0.00001
 model = MultiTaskVAE(input_dim, latent_dim).to(device)
 optimizer = optim.Adam(model.parameters(), lr=lr)
 
-# initializations required for GradNorm
-Weightloss1 = torch.tensor([1.0], requires_grad=True, device=device)
-Weightloss2 = torch.tensor([1.0], requires_grad=True, device=device)
-params = [Weightloss1, Weightloss2]
-scaling_optimizer = optim.Adam(params, lr=lr_scaling)
-Gradloss = L1Loss()
-alpha = 0.8
-l0_recon = None
-l0_purity = None
-
+# weights
+kl_loss_weight = 0.00001
+recon_loss_weight = 0.8
+purity_loss_weight = 1 - recon_loss_weight
+alpha = None
 
 # loss functions
 def KLD(mu, sigma):
@@ -133,10 +127,7 @@ def epochs_loop(train_loader, val_loader, train_set, val_set, comment):
             if r2_model_file is not None:
                 old_file = pathlib.Path(r2_model_file)
                 old_file.unlink()
-            print(f"model dir: {model_dir}")
-            print(f"/best_r2_model_{metrics['val_R2'][-1]}.pt")
             r2_model_file = model_dir + f"/best_r2_model_{metrics['val_R2'][-1]}.pt"
-            print(r2_model_file)
             torch.save(model, r2_model_file)
         if is_lowest_score(["val_loss"]):
             if loss_model_file is not None:
@@ -149,9 +140,6 @@ def epochs_loop(train_loader, val_loader, train_set, val_set, comment):
 
 
 def batch_train_loop(train_loader, epoch):
-    global params
-    global l0_recon
-    global l0_purity
     model.train()
     total_loss = 0
     total_r2 = 0
@@ -168,62 +156,17 @@ def batch_train_loop(train_loader, epoch):
         x, w = x.to(device), w.to(device)
 
         x_hat, w_hat, mu, sigma = model(x)
-        recon_loss = params[0] * MSE(x_hat, x)
-        purity_loss = params[1] * MSE(w_hat, w)
+        recon_loss = recon_loss_weight * MSE(x_hat, x)
+        purity_loss = purity_loss_weight * MSE(w_hat, w)
         kl_loss = KLD(mu, sigma) * kl_loss_weight
         loss = torch.div(torch.add(recon_loss, purity_loss), 2) + kl_loss
 
-        # for the first epoch with no l0
-        if epoch == 0:
-            l0_recon = recon_loss.data
-            l0_purity = purity_loss.data
-
         optimizer.zero_grad()
 
-        loss.backward(retain_graph=True)
-
-        last_common_layer = model.encoder.fc.weight
-
-        #TODO: check why G1 and G2 are Python floats instead of pytorch tensors and if the warning
-        # "Using a target size (torch.Size([1])) that is different to the input size (torch.Size([]))" is a problem
-
-        # Getting gradients of the first layers of each task and calculate the gradients l2-norm
-        G1R = torch.autograd.grad(recon_loss, last_common_layer, retain_graph=True, create_graph=True)
-        G1 = torch.norm(G1R[0], 2)
-        G2R = torch.autograd.grad(purity_loss, last_common_layer, retain_graph=True, create_graph=True)
-        G2 = torch.norm(G2R[0], 2)
-        G_avg = torch.div(torch.add(G1, G2), 2)
-
-        # Calculating relative losses
-        lhat1 = torch.div(recon_loss, l0_recon)
-        lhat2 = torch.div(purity_loss, l0_purity)
-        lhat_avg = torch.div(torch.add(lhat1, lhat2), 2)
-
-        # Calculating relative inverse training rates for tasks
-        inv_rate1 = torch.div(lhat1,lhat_avg)
-        inv_rate2 = torch.div(lhat2,lhat_avg)
-
-        # Calculating the constant target for Eq. 2 in the GradNorm paper
-        C1 = G_avg*(inv_rate1)**alpha
-        C2 = G_avg*(inv_rate2)**alpha
-        C1 = C1.detach()
-        C2 = C2.detach()
-
-        scaling_optimizer.zero_grad()
-
-        # Calculating the gradient loss according to Eq. 2 in the GradNorm paper
-        Lgrad = torch.add(Gradloss(G1, C1),Gradloss(G2, C2))
-        Lgrad.backward()
-
-        # Updating loss weights
-        scaling_optimizer.step()
+        loss.backward()
 
         # Updating model weights
         optimizer.step()
-
-        # Renormalizing the losses weights
-        coef = 2/torch.add(Weightloss1, Weightloss2)
-        params = [coef*Weightloss1, coef*Weightloss2]
 
 
         total_loss += loss.item() / count
@@ -231,8 +174,8 @@ def batch_train_loop(train_loader, epoch):
         total_reg_loss += purity_loss.item() / count
         total_recon_loss += recon_loss.item() / count
         total_r2 += float(r2_score(x.detach().cpu(), x_hat.detach().cpu())) / count
-        total_w1 += float(params[0].detach().cpu()) / count
-        total_w2 += float(params[1].detach().cpu()) / count
+        total_w1 += float(recon_loss_weight) / count
+        total_w2 += float(purity_loss_weight) / count
 
 
     return total_loss, total_recon_loss, total_reg_loss, total_kl_loss, total_r2, total_w1, total_w2
@@ -245,8 +188,6 @@ def batch_val_loop(val_loader):
     total_reg_loss = 0
     total_recon_loss = 0
     count = len(val_loader)
-    recon_loss_weight = float(params[0].detach().cpu())
-    reg_head_weight = float(params[1].detach().cpu())
 
     with torch.no_grad():
         for batch in val_loader:
@@ -255,7 +196,7 @@ def batch_val_loop(val_loader):
 
             x_hat, w_hat, mu, sigma = model(x)
             recon_loss = recon_loss_weight * MSE(x_hat, x)
-            purity_loss = reg_head_weight * MSE(w_hat, w)
+            purity_loss = purity_loss_weight * MSE(w_hat, w)
             kl_loss = KLD(mu, sigma) * kl_loss_weight
             loss = torch.div(torch.add(recon_loss, purity_loss), 2) + kl_loss
 

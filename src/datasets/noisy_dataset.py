372022ecf3b54e116beb784ed5fd076652c10d69
Felix Schroeder <felix.schroeder@tum.de>
2024-10-08 18:19:11 +0200

    addapted noisy_dataset to multitask model architecture

--- src/datasets/noisy_dataset.py
+++ src/datasets/noisy_dataset.py
@@ -8,13 +8,14 @@ class NoisyDataset(Dataset):
     """
     a Dataset that adds gaussian noise with a standard deviation of std to each sample in the dataset
     """
-    def __init__(self, data: pd.DataFrame, var, transform=None):
-        super().__init__(data, transform)
+    def __init__(self, genes: pd.DataFrame, meta: pd.DataFrame, var, transform=None):
+        super().__init__(genes, meta, transform)
         self.var = var
 
     def __getitem__(self, index):
-        tensor = torch.tensor(self.data[index], dtype=torch.float32)
-        return self.add_gaussian_noise(tensor), self.indices[index]
+        gene_exp = torch.tensor(self.genes[index], dtype=torch.float32)
+        purity = torch.tensor([self.meta["cancer_purity"].iloc[index]], dtype=torch.float32)
+        return self.add_gaussian_noise(gene_exp), purity, self.indices[index]
 
     def add_gaussian_noise(self, data):
         noise = (self.var**0.5)*torch.randn(data.shape)
